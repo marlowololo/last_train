eda3daa7e846fa2d69f92bbb3558c8aba427a291
mikhaelmartin <mikhael.martin@gmail.com>
2020-02-01 01:33:40 +0700

    HorizontalMove

---
+++ Assets/Scripts/HorizontalMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorizontalMove : MonoBehaviour
+{
+    
+
+    [Range(0,5)] [SerializeField]  float speed;
+    [SerializeField] Direction direction;
+    [SerializeField]  enum Direction
+    {
+        left=-1,
+        right=1
+    }
+
+    Rigidbody2D rigidBody;
+    
+    private void Start() 
+    {
+        rigidBody=GetComponent<Rigidbody2D>();
+        Vector2 force = new Vector2(speed*(float)direction,0);        
+        rigidBody.velocity=force;  
+    }
+  }
