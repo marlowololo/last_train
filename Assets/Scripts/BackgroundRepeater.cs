08ab5fecb06e222765fde803e5b93edd0e2f841e
mikhaelmartin <mikhael.martin@gmail.com>
2020-02-01 02:18:54 +0700

    BackgroundRepeater

---
+++ Assets/Scripts/BackgroundRepeater.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundRepeater : MonoBehaviour
+{
+    float screenSizeX;
+    float spriteSizeX;
+    bool isHaveClone=false;
+    GameObject clone;
+
+    private void Start() 
+    {
+        Camera cam = Camera.main;
+        float height = 2f * cam.orthographicSize;
+        screenSizeX = height * cam.aspect;
+        spriteSizeX = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    private void Update() 
+    {
+        if(!isHaveClone)
+        {
+            if(transform.position.x+0.5*spriteSizeX<=0.5*screenSizeX)
+            {
+                isHaveClone=true;
+                clone = Instantiate(gameObject,transform.position+new Vector3(spriteSizeX,0,0),Quaternion.identity);
+                clone.name = "c";
+            }
+        }
+        
+        if(transform.position.x+0.5*spriteSizeX<=-0.5*screenSizeX)
+        {
+            
+            Destroy(gameObject);
+        }
+
+    }
+}
+
