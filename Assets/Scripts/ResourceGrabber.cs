ea99c9ba5515a1909231a41c4da682fa7bd90cf1
insan.athoriq <eon@agate.id>
2020-02-02 01:15:58 +0700

    add animation on collecting resource

--- Assets/Scripts/ResourceGrabber.cs
+++ Assets/Scripts/ResourceGrabber.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using DragonBones;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,11 +8,15 @@ public class ResourceGrabber : MonoBehaviour
     [SerializeField] Collider2D triggerCollider;
     [SerializeField] string resourceTag;
     [SerializeField] ResourceInventoryScriptable resourceInventoryAsset;
+    [SerializeField] BaseDoor baseDoor;
+
+    private UnityArmatureComponent currentWorker;
 
     private void OnTriggerEnter2D(Collider2D other) 
     {
         if(other.gameObject.CompareTag(resourceTag))
         {
+            baseDoor.TriggerAnimation();
             resourceInventoryAsset.resource +=1;
             Destroy(other.gameObject);
         }
--- Assets/Scripts/Train/BaseDoor.cs
+++ Assets/Scripts/Train/BaseDoor.cs
@@ -1,11 +1,23 @@
-﻿using System.Collections;
+﻿using DragonBones;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BaseDoor : MonoBehaviour
 {
+
+    [SerializeField] ResourceGrabber resourceGrabber;
+    private UnityArmatureComponent currentWorker;
+
     public void OnClick()
     {
+        currentWorker = GameManager.Instance.selectedWorker.GetSelectedWorkerUAC();
         GameManager.Instance.selectedWorker.MoveTo(gameObject);
+    }
+
+    public void TriggerAnimation()
+    {
+        currentWorker.animation.Play("ForageUp1");
     }
 }
