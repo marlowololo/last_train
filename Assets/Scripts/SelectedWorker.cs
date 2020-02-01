2116583876b2c63c13035f837dc4c53b5d57e346
insan.athoriq <eon@agate.id>
2020-02-01 21:36:43 +0700

    add cursor to selected worker

--- Assets/Scripts/SelectedWorker.cs
+++ Assets/Scripts/SelectedWorker.cs
@@ -6,10 +6,21 @@ using UnityEngine;
 public class SelectedWorker : MonoBehaviour
 {
     [SerializeField] GameObject selectedWorker;
+    [SerializeField] GameObject selectedMarkerPrefab;
+    GameObject selectedMarker;
+
+    Vector3 markerDefaultOffset = new Vector3(0, 22f, 0);
+
+    private void Start()
+    {
+        selectedMarker = Instantiate(selectedMarkerPrefab);
+    }
 
     public void SetSelectedWorker(GameObject worker)
     {
         selectedWorker=worker;
+        selectedMarker.transform.SetParent(worker.transform);
+        selectedMarker.transform.localPosition = markerDefaultOffset;
     }
 
     public void MoveTo(GameObject targetObject)
