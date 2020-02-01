977901a9791264fb260bcf361594cd212accfb66
mikhaelmartin <mikhael.martin@gmail.com>
2020-02-01 13:56:50 +0700

    simple worker movement and sprite update

--- Assets/PlayableWorkerMovement.cs
+++ Assets/PlayableWorkerMovement.cs
@@ -4,35 +4,60 @@ using UnityEngine;
 
 public class PlayableWorkerMovement : MonoBehaviour
 {
-    [Range(0,5)] [SerializeField] float speed=1;
+    [Range(0,10)] [SerializeField] float speed=1;
+    [Range(0,0.5f)] [SerializeField] float offset;
     Rigidbody2D rb;
+    public Vector2 currentPosition;
+    public Vector2 targetPosition;
+    float yPosition;
+    public float direction=1;
+
     private void Start() 
     {
         rb = GetComponent<Rigidbody2D>();
+        rb.velocity=Vector2.zero;
+        yPosition = transform.position.y;
+        targetPosition = currentPosition;
     }
     public void SetYPosition()
     {
 
     }
-
     private void Update() 
     {
         if(Input.GetKeyDown(KeyCode.Mouse0))
+        {            
+            targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            targetPosition = new Vector2(targetPosition.x,yPosition); 
+            direction = Mathf.Sign(targetPosition.x-currentPosition.x);
+            rb.velocity=new Vector2(direction*speed,0);
+
+        }
+
+        currentPosition = transform.position;
+
+        if(direction==1)
         {
-            Vector2 currentPosition = transform.position;
-            Vector2 targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition); 
-            if(targetPosition.x-currentPosition.x!=0)
-            {
-                rb.velocity= new Vector2(Mathf.Sign(targetPosition.x-currentPosition.x) * speed,0);
-            }
-            else
+           
+            if(currentPosition.x>targetPosition.x)
             {
                 rb.velocity=Vector2.zero;
             }
-            
-            
-        }    
+                        
+        }
+        else if(direction==-1)
+        {
+            if(currentPosition.x<targetPosition.x)
+            {
+                rb.velocity=Vector2.zero;
+            }
+         
+        }
+        
+           
     }
+
+
 
 
 }
---
+++ Assets/PlayableWorkerSpriteUpdate.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayableWorkerSpriteUpdate : MonoBehaviour
+{
+    SpriteRenderer sprite;
+    PlayableWorkerMovement movement;
+
+    private void Start() 
+    {
+        sprite = GetComponent<SpriteRenderer>();    
+        movement = GetComponent<PlayableWorkerMovement>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(movement.direction==1)
+        {
+            sprite.flipX=false;
+        }
+        else if(movement.direction==-1)
+        {
+            sprite.flipX=true;
+        }
+    }
+}
