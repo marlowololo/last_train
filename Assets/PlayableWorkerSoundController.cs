bdfd98944c28070548c669ba4c4ca01a9147d5a2
mikhaelmartin <mikhael.martin@gmail.com>
2020-02-02 14:38:56 +0700

    prefabwork + sound

---
+++ Assets/PlayableWorkerSoundController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DragonBones;
+
+public class PlayableWorkerSoundController : MonoBehaviour
+{
+    [SerializeField] AudioClip clickSound;
+    [SerializeField] AudioClip fixingSound;
+    [SerializeField] UnityArmatureComponent armatureComponent;
+
+    [SerializeField] AudioSource audioSource;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!audioSource.isPlaying)
+        {
+            if(armatureComponent.animationName=="fixing")
+            {
+                audioSource.clip=fixingSound;
+                audioSource.Play();
+            }
+            else
+            {
+                audioSource.clip=clickSound;
+            }
+
+        }
+        
+    }
+}
