48223dfb9a74d94fc009cf184ac33c9d4e506787
mikhaelmartin <mikhael.martin@gmail.com>
2020-02-02 10:51:01 +0700

    RoofResourceSpawner

--- Assets/RoofResourceSpawner.cs
+++ Assets/RoofResourceSpawner.cs
@@ -6,16 +6,16 @@ public class RoofResourceSpawner : MonoBehaviour
 {
     [SerializeField] GameObject resourcePrefab;
     [SerializeField] bool runAtStart=false;
-    [Range(-20,20)] [SerializeField] float xPosition=0;
-    [Range(-20,20)] [SerializeField] float yPosition=0;    
+    // [Range(-20,20)] [SerializeField] float xPosition=0;
+    // [Range(-20,20)] [SerializeField] float yPosition=0;    
     [Range(0,20)] [SerializeField] float timeInterval=1;
 
     [Header("Setting for random values. Random.Range(value,value+deviation). 0 = not using random")]
-    [Tooltip("Output value =  Random.Range(value,value+deviation)\n0 = not using random")] 
-    [Range(0,10)] [SerializeField] float randomXPositionDeviation=0;
+    // [Tooltip("Output value =  Random.Range(value,value+deviation)\n0 = not using random")] 
+    // [Range(0,10)] [SerializeField] float randomXPositionDeviation=0;
 
-    [Tooltip("Output value =  Random.Range(value,value+deviation)\n0 = not using random")]   
-    [Range(0,10)] [SerializeField] float randomYPositionDeviation=0;
+    // [Tooltip("Output value =  Random.Range(value,value+deviation)\n0 = not using random")]   
+    // [Range(0,10)] [SerializeField] float randomYPositionDeviation=0;
 
     [Tooltip("Output value =  Random.Range(value,value+deviation)\n0 = not using random")]      
     [Range(0,10)] [SerializeField] float randomTimeIntervalDeviation=0;
@@ -23,7 +23,7 @@ public class RoofResourceSpawner : MonoBehaviour
     float boundary;
     private void Start() 
     {
-        boundary = GetComponent<SpriteRenderer>().bounds.max.x;
+        boundary = GetComponent<SpriteRenderer>().bounds.size.x/2;
         if(runAtStart)
         {
             StartCoroutine(Spawn()); 
@@ -34,12 +34,14 @@ public class RoofResourceSpawner : MonoBehaviour
     {
         while(true)
         {
-            float newXPosition = Random.Range(xPosition,xPosition+randomXPositionDeviation);
-            float newYPosition = Random.Range(yPosition,yPosition+randomYPositionDeviation);
+            // float newXPosition = Random.Range(xPosition,xPosition+randomXPositionDeviation);
+            // float newYPosition = Random.Range(yPosition,yPosition+randomYPositionDeviation);
             float newTimeInterval = Random.Range(timeInterval,timeInterval+randomTimeIntervalDeviation);
             Vector2 spawnPosition = new Vector2(Random.Range(-boundary,boundary),transform.position.y);
             //transform.position = new Vector2(newXPosition,newYPosition);
-            Instantiate(resourcePrefab,spawnPosition,Quaternion.identity, this.transform);
+            GameObject resource = Instantiate(resourcePrefab, this.transform);
+            resource.transform.localPosition = spawnPosition;
+
             yield return new WaitForSecondsRealtime(newTimeInterval);
         }
     }
