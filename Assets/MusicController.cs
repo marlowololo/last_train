664e1ef4955dd45f971e2b1e6c9807c649ff6c9d
mikhaelmartin <mikhael.martin@gmail.com>
2020-02-02 15:27:17 +0700

    MusicController

---
+++ Assets/MusicController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicController : MonoBehaviour
+{
+    [SerializeField] AudioSource audioSource;
+    [SerializeField] AudioClip clip1;
+    [SerializeField] AudioClip clip2;
+    [SerializeField] AudioClip clip3;
+    [SerializeField] AudioClip clip4;
+    [SerializeField] AudioClip pausedClip;
+
+    int current=1;
+    private void Start() 
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void Update() 
+    {
+        if(!(current == GameManager.Instance.currentPhase))
+        {
+            if(GameManager.Instance.currentPhase==1)
+            {
+                current =  GameManager.Instance.currentPhase;
+                audioSource.clip=clip1;
+                audioSource.Play();
+            }
+            else if(GameManager.Instance.currentPhase==2)
+            {
+                current =  GameManager.Instance.currentPhase;
+                audioSource.clip=clip2;
+                audioSource.Play();
+            }
+            else if(GameManager.Instance.currentPhase==3)
+            {
+                current =  GameManager.Instance.currentPhase;
+                audioSource.clip=clip3;
+                audioSource.Play();
+            }
+            else if(GameManager.Instance.currentPhase==4)
+            {
+                current =  GameManager.Instance.currentPhase;
+                audioSource.clip=clip4;
+                audioSource.Play();
+            }
+        }
+     
+
+
+
+    }
+}
